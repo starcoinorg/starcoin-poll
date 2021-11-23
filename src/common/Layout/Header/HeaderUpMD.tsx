500c29fb2933f39f013f0624d338e15c9fd60142
bing <tarvos21@gmail.com>
2021-11-24 03:25:08 +0800

    Add connect wallet & install wallet button to the top-left header on pc device

--- src/common/Layout/Header/HeaderUpMD.tsx
+++ src/common/Layout/Header/HeaderUpMD.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
+import { LANGUAGES_LABEL } from '@/utils/constants';
+import { observer } from 'mobx-react';
 import { createStyles, Theme, withStyles } from '@material-ui/core/styles';
 import classNames from 'classnames';
 import BaseRouteLink from '@/common/BaseRouteLink';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
+import Box from '@material-ui/core/Box';
 import MenuItem from '@material-ui/core/MenuItem';
 import Tooltip from '@material-ui/core/Tooltip';
 import LanguageIcon from '@material-ui/icons/Translate';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { LANGUAGES_LABEL } from '@/utils/constants';
+import StarMaskOnboarding from '@starcoin/starmask-onboarding';
 import Networks from '../../Networks';
 import Tabs from './Tabs';
+import { useStores } from '../../../useMobxStore';
 
 const useStyles = (theme: Theme) =>
   createStyles({
@@ -54,9 +58,23 @@ const useStyles = (theme: Theme) =>
     noUpperCase: {
       textTransform: 'none',
     },
+    rightBox: {
+      marginLeft: 'auto',
+    },
     button: {
       height: theme.spacing(6),
       border: 'none',
+    },
+    buttonStyle: {
+      borderColor: '#1C4BDE',
+      borderRadius: '25px',
+      marginRight: '0.3rem'
+    },
+    darkBgButton: {
+      color: '#000',
+      backgroundColor: '#F7F9FA',
+      borderRadius: '25px',
+      marginRight: '0.3rem'
     },
     search: {
       alignItems: 'center',
@@ -104,6 +122,75 @@ function Index(props: any) {
   const handleLanguageIconClick = (event: any) => {
     setLanguageMenu(event.currentTarget);
   };
+
+  const [accountStatus, setAccountStatus] = useState(-1)
+  const [accountAddress, setAccountAddress] = useState('')
+  const [network, setNetwork] = useState('')
+  const { AccountStore } = useStores()
+
+  const isStarMaskInstalled = StarMaskOnboarding.isStarMaskInstalled();
+
+  useEffect(() => {
+    // console.log(window.starcoin && window.starcoin.selectedAddress,  window.starcoin.selectedAddress)
+    if (window.starcoin && window.starcoin.selectedAddress) {
+      setAccountAddress(window.starcoin.selectedAddress)
+      setAccountStatus(1)
+    // } else if (AccountStore.isInstall) {
+    } else if (isStarMaskInstalled) {
+      setAccountStatus(0)
+    } else {
+      setAccountStatus(-1)
+    }
+  }, [AccountStore.isInstall, AccountStore.accountStatus])
+
+  function handleNewAccounts(accounts: any) {
+    if (accounts.length === 0) {
+      setAccountStatus(0)
+      setAccountAddress("")
+    } else {
+      setAccountAddress(accounts[0])
+    }
+  }
+
+  function handleNewNetwork(network: any) {
+    setNetwork(network)
+  }
+
+  if (window.starcoin) {
+    window.starcoin.on('accountsChanged', handleNewAccounts)
+    window.starcoin.on('networkChanged', handleNewNetwork)
+  }
+
+
+  useEffect(() => {
+    if (window.starcoin && window.starcoin.networkVersion) {
+      setNetwork(window.starcoin && window.starcoin.networkVersion)
+    }
+  }, [])
+
+
+
+  useEffect(() => {
+    if (window.starcoin && window.starcoin.selectedAddress) {
+      setAccountAddress(window.starcoin.selectedAddress)
+    }
+  }, [])
+
+  async function connectWallet() {
+    if (accountStatus === 0) {
+      window.starcoin.request({
+        method: 'stc_requestAccounts',
+      }).then((res: any) => {
+        if (res.length > 0) {
+          setAccountStatus(1)
+          setAccountAddress(res[0] || '')
+          AccountStore.setCurrentAccount(res[0] || '')
+        }
+      })
+    } else if (accountStatus === -1) {
+      window.open("https://chrome.google.com/webstore/detail/starmask/mfhbebgoclkghebffdldpobeajmbecfk")
+    }
+  }
 
   const handleLanguageMenuClose = (lang?: string) => {
     if (lang) {
@@ -195,6 +282,16 @@ function Index(props: any) {
           <Networks />
           {i18nMenu}
         </div>
+        <Box display="flex" alignItems="center" className={classes.rightBox}>
+          {accountStatus === 1 ? <Button variant="outlined" className={classes.darkBgButton}>
+            {AccountStore.networkVersion[window.starcoin.networkVersion]}
+          </Button> : null}
+          <Button variant="contained" color="primary" className={classes.buttonStyle} onClick={connectWallet}>
+            {accountStatus === -1 ? t('poll.installWallet') : ''}
+            {accountStatus === 0 ? t('poll.connectWallet') : ''}
+            {accountStatus === 1 ? `${accountAddress.substr(0, 4)}....${accountAddress.substring(accountAddress.length - 4)}` : ''}
+          </Button>
+        </Box>
       </div>
     </div>
   );
@@ -204,4 +301,4 @@ Index.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(useStyles)(Index);
+export default observer(withStyles(useStyles)(Index));
